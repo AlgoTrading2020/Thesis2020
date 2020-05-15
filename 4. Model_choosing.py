f578ed623e5c0bedfffcf5a138bec8486d12907b
AlgoTrading2020 <65388988+AlgoTrading2020@users.noreply.github.com>
2020-05-15 12:13:29 +0200

    Add files via upload

--- 4. Model_choosing.py
+++ 4. Model_choosing.py
@@ -19,31 +19,146 @@ df = read_data()
 # In[3]:
 
 
-get_ipython().run_cell_magic('time', '', "from sklearn.linear_model import LogisticRegression\n\nX = df.text\ny = df.alpha_label\n\nX_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)\n\n#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.\npipe = Pipeline([('tfidf', TfidfVectorizer()),\n                 ('clf', LogisticRegression(random_state = 42)),\n                ])\n\ngrid = cross_validate(pipe, , cv=5)\ngrid.fit(X_train, y_train)\n\nprint('Best cross-validation accuracy: %s' % grid.best_score_)\n\nprint(y_train.value_counts(' '))\n\ndifference = grid.best_score_ - y_train.value_counts(' ')[0]\nprint('Difference between accuracy and baseline: ', difference)\nprint(' ')")
+
+from sklearn.linear_model import LogisticRegression
+
+X = df.text
+y = df.alpha_label
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)
+
+#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.
+pipe = Pipeline([('tfidf', TfidfVectorizer()),
+                 ('clf', LogisticRegression(random_state = 42)),
+                ])
+
+grid = cross_validate(pipe, , cv=5)
+grid.fit(X_train, y_train)
+
+print('Best cross-validation accuracy: %s' % grid.best_score_)
+
+print(y_train.value_counts(' '))
+
+difference = grid.best_score_ - y_train.value_counts(' ')[0]
+print('Difference between accuracy and baseline: ', difference)
+print(' ')
 
 
 # In[4]:
 
 
-get_ipython().run_cell_magic('time', '', "from sklearn.naive_bayes import MultinomialNB\n\nX = df.text\ny = df.alpha_label\n\nX_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)\nparams = {}\n\n#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.\npipe = Pipeline([('tfidf', TfidfVectorizer()),\n                 ('clf', MultinomialNB()),\n                ])\n\ngrid = GridSearchCV(pipe, params, cv=5)\ngrid.fit(X_train, y_train)\n\nprint('Best cross-validation accuracy: %s' % grid.best_score_)\n\nprint(y_train.value_counts(' '))\n\ndifference = grid.best_score_ - y_train.value_counts(' ')[0]\nprint('Difference between accuracy and baseline: ', difference)\nprint(' ')")
+
+from sklearn.naive_bayes import MultinomialNB
+
+X = df.text
+y = df.alpha_label
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)
+params = {}
+
+#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.
+pipe = Pipeline([('tfidf', TfidfVectorizer()),
+                 ('clf', MultinomialNB()),
+                ])
+
+grid = GridSearchCV(pipe, params, cv=5)
+grid.fit(X_train, y_train)
+
+print('Best cross-validation accuracy: %s' % grid.best_score_)
+
+print(y_train.value_counts(' '))
+
+difference = grid.best_score_ - y_train.value_counts(' ')[0]
+print('Difference between accuracy and baseline: ', difference)
+print(' ')
 
 
 # In[3]:
 
 
-get_ipython().run_cell_magic('time', '', "from sklearn.linear_model import SGDClassifier\nparams = {'clf__random_state':[42]}\n\nX = df.text\ny = df.alpha_label\n\nX_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)\n\n#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.\npipe = Pipeline([('tfidf', TfidfVectorizer()),\n                 ('clf', SGDClassifier()),\n                ])\n\ngrid = GridSearchCV(pipe, params, cv=5)\ngrid.fit(X_train, y_train)\n\nprint('Best cross-validation accuracy: %s' % grid.best_score_)\n\nprint(y_train.value_counts(' '))\n\ndifference = grid.best_score_ - y_train.value_counts(' ')[0]\nprint('Difference between accuracy and baseline: ', difference)\nprint(' ')")
+
+from sklearn.linear_model import SGDClassifier
+params = {'clf__random_state':[42]}
+
+X = df.text
+y = df.alpha_label
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)
+
+#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.
+pipe = Pipeline([('tfidf', TfidfVectorizer()),
+                 ('clf', SGDClassifier()),
+                ])
+
+grid = GridSearchCV(pipe, params, cv=5)
+grid.fit(X_train, y_train)
+
+print('Best cross-validation accuracy: %s' % grid.best_score_)
+
+print(y_train.value_counts(' '))
+
+difference = grid.best_score_ - y_train.value_counts(' ')[0]
+print('Difference between accuracy and baseline: ', difference)
+print(' ')
 
 
 # In[4]:
 
 
-get_ipython().run_cell_magic('time', '', "from sklearn.svm import LinearSVC\nparams = {'clf__random_state':[42]}\n\nX = df.text\ny = df.alpha_label\n\nX_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)\n\n#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.\npipe = Pipeline([('tfidf', TfidfVectorizer()),\n                 ('clf', LinearSVC()),\n                ])\n\ngrid = GridSearchCV(pipe, params, cv=5)\ngrid.fit(X_train, y_train)\n\nprint('Best cross-validation accuracy: %s' % grid.best_score_)\n\nprint(y_train.value_counts(' '))\n\ndifference = grid.best_score_ - y_train.value_counts(' ')[0]\nprint('Difference between accuracy and baseline: ', difference)\nprint(' ')")
+
+from sklearn.svm import LinearSVC
+params = {'clf__random_state':[42]}
+
+X = df.text
+y = df.alpha_label
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)
+
+#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.
+pipe = Pipeline([('tfidf', TfidfVectorizer()),
+                 ('clf', LinearSVC()),
+                ])
+
+grid = GridSearchCV(pipe, params, cv=5)
+grid.fit(X_train, y_train)
+
+print('Best cross-validation accuracy: %s' % grid.best_score_)
+
+print(y_train.value_counts(' '))
+
+difference = grid.best_score_ - y_train.value_counts(' ')[0]
+print('Difference between accuracy and baseline: ', difference)
+print(' ')
 
 
 # In[2]:
 
 
-get_ipython().run_cell_magic('time', '', "\nfrom sklearn.svm import SVC\nparams = {'clf__random_state':[42]}\n\nX = df.text\ny = df.alpha_label\n\nX_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)\n\n#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.\npipe = Pipeline([('tfidf', TfidfVectorizer()),\n                 ('clf', SVC()),\n                ])\n\ngrid = GridSearchCV(pipe, params, cv=5)\ngrid.fit(X_train, y_train)\n\nprint('Best cross-validation accuracy: %s' % grid.best_score_)\n\nprint(y_train.value_counts(' '))\n\ndifference = grid.best_score_ - y_train.value_counts(' ')[0]\nprint('Difference between accuracy and baseline: ', difference)\nprint(' ')")
+
+
+from sklearn.svm import SVC
+params = {'clf__random_state':[42]}
+
+X = df.text
+y = df.alpha_label
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, stratify=y)
+
+#TfidfVectorizer (1) vectorizes and computes TF, (2) computes IDF and (3) computes TFIDF for each word.
+pipe = Pipeline([('tfidf', TfidfVectorizer()),
+                 ('clf', SVC()),
+                ])
+
+grid = GridSearchCV(pipe, params, cv=5)
+grid.fit(X_train, y_train)
+
+print('Best cross-validation accuracy: %s' % grid.best_score_)
+
+print(y_train.value_counts(' '))
+
+difference = grid.best_score_ - y_train.value_counts(' ')[0]
+print('Difference between accuracy and baseline: ', difference)
+print(' ')
 
 
 # In[ ]:
